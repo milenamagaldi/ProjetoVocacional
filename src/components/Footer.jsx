9e9dc8f11533236d44c24fd02a39e060b736e24f
milenamagaldi <milenamagaldimoura@gmail.com>
2026-03-25 10:07:44 -0300

    third commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ function Footer() {
       <div className="footer-content">
         <p>&copy; 2026 Pastoral Vocacional - Diocese de Nova Iguaçu. Todos os direitos reservados.</p>
         <div className="social">
-          <a href="#" target="_blank">Facebook</a>
-          <a href="#" target="_blank">Instagram</a>
-          <a href="#" target="_blank">YouTube</a>
+          <a href="https://www.facebook.com/diocesedenovaiguacu/?locale=pt_BR" target="_blank">Facebook - Diocese de Nova Iguaçu</a>
+          <a href="https://www.instagram.com/vocacionalsav/" target="_blank">Instagram - VocacionalSav</a>
+          <a href="https://www.youtube.com/@Mission%C3%A1riasCordimarianas" target="_blank">YouTube - Missionárias Cordimarianas</a>
         </div>
       </div>
     </footer>
